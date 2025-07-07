b2af5b8f295944e0ff998bf69d32f5e865d60767
tdestouches <thomas.destouches@dnalytics.com>
2025-07-07 15:55:14 +0200

    add formatter for R code

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -229,6 +229,7 @@ return {
         local ensure_installed = vim.tbl_keys(servers or {})
         vim.list_extend(ensure_installed, {
             'stylua', -- Used to format Lua code
+            'air', -- for R code
         })
         require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
